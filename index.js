ace67cd2176e0f490ba8a2cb5bc4bcaf939e337b
ssk42 <sthreitz@gmail.com>
2021-05-09 20:36:12 -0400

    variable renaming

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on('message', message => {
 
     const args = message.content.slice(cookie.length).trim().split(' ');
     const cookieEmoji= ':cookie:';
-    const taggedUsers=message.mentions.users;
+    const mentionedUsers=message.mentions.users;
     const givenCookieMessage=': you have been given a cookie. Enjoy the chocolate-y goodness.';
     const botMessage='You cannot send cookies to bots. Bots cannot enjoy the ooey or the gooey of a cookie.';
 
@@ -21,36 +21,34 @@ client.on('message', message => {
         return message.channel.send(`You didn't provide any users to give a cookie to, ${message.author}!`);
     }
 	else if (message.content.startsWith(cookie)) {
-        let taggedUser='';
+        let usersToTag='';
         
-        if (taggedUsers.size===1){
-            taggedUser = taggedUsers.first();
-            if (!taggedUser.bot) {
-                message.channel.send('<@'+taggedUser.id+'>'+givenCookieMessage);
+        if (mentionedUsers.size===1){
+            usersToTag = mentionedUsers.first();
+            if (!usersToTag.bot) {
+                message.channel.send('<@'+usersToTag.id+'>'+givenCookieMessage);
             } else {
                 message.channel.send(botMessage);
             }
             return;
         }
         else{
-            for (let [user,userInfo] of taggedUsers) {
+            for (let [userId,userInfo] of mentionedUsers) {
                 if(!userInfo.bot){
-                    let tagOfUser='';
+                    let currentUserTag='<@'+userId+'>';
                     
-                    if(taggedUser===''){
-                        tagOfUser='<@'+user+'>';
-                        taggedUser = tagOfUser;
+                    if(usersToTag===''){
+                        usersToTag = currentUserTag;
                     }
                     else{
-                        tagOfUser='<@'+user+'>';
-                        taggedUser = taggedUser+', '+tagOfUser;
+                        usersToTag = usersToTag+', '+currentUserTag;
                     }
                 }
                 else{
                     message.channel.send(botMessage);
                 }
             }
-            message.channel.send(taggedUser+givenCookieMessage);
+            message.channel.send(usersToTag+givenCookieMessage);
         }
 	}
 });
